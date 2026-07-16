f1df5026ef20b779ba6bb99a1d7e2862ff2532e0
Subomi Obans <subomi.obans3@gmail.com>
2026-07-16 13:57:41 +0100

    Add Dashboard Overview endpoint with weekly report counts

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SystemLogListView
+from .views import SystemLogListView, DashboardOverviewView
 
 urlpatterns = [
     path('logs/', SystemLogListView.as_view(), name='system-logs'),
+    path('overview/', DashboardOverviewView.as_view(), name='dashboard-overview')
 ]
